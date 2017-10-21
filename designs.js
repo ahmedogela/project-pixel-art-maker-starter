6b37d75821809e99dbd2cc8256829a426387691b
Ahmed Ogela <ogela@appatlab.com>
2017-10-21 17:57:07 +0200

    create grid function

--- designs.js
+++ designs.js
@@ -3,3 +3,13 @@ let colorInput = $('#colorPicker');
 let heightInput = $('#input_height');
 let widthInput = $('#input_width');
 let table = $('#pixel_canvas');
+
+// Grid Creation
+function makeGrid() {
+    for(let i = 0; i < heightInput.val(); i++) {
+        table.append('<tr id="row' + i +'"></tr>');
+        for(let j = 0; j < widthInput.val(); j++) {
+            $('#row' + i).append('<td></td>');
+        }
+    }
+}
